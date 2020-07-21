c9088cbf643369a8be56bc0ff38d36bde09177c1
Thomas Duigou <thomas.duigou@inrae.fr>
2020-07-21 14:39:45 +0200

    renaming package iv

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
 setuptools.setup(
-        name="chemtools",
-        version="0.2",
-        author="Thomas Duigou",
-        author_email="thomas.duigou@inrae.fr",
-        description="Toolbox to deal with chemicals",
+        name='rpchemtools',
+        version='0.3.0',
+        author='Thomas Duigou',
+        author_email='thomas.duigou@inrae.fr',
+        description='Toolbox to deal with chemicals',
         packages=setuptools.find_packages()
 )
